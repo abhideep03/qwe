5d6ac9de5e12c2b3497db7611005d6114bd92264
unknown <abhideep.2004.3@gmail.com>
2024-08-07 00:46:22 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Home from "./components/home";
+import Home from "./components/Home";
 
 function App() {
   return (
